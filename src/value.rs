479ab975834f1dd7c5bbfe79be23ee918ea13a13
K Henriksson <khenriksson@bgsh.org>
2019-06-25 19:06:28 -0700

    Add flag support for f32 and f64 values
    Fixes #7.

--- src/value.rs
+++ src/value.rs
@@ -67,4 +67,4 @@ macro_rules! impl_value_for_primitive {
     };
 }
 
-impl_value_for_primitive!(u8 u16 u32 u64 u128 usize i8 i16 i32 i64 i128 isize);
+impl_value_for_primitive!(u8 u16 u32 u64 u128 usize i8 i16 i32 i64 i128 isize f32 f64);
