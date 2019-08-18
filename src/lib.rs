e96e1094eb1881fa63adfdc5b55834eca0bdd2f4
David Tolnay <dtolnay@gmail.com>
2019-08-17 19:18:05 -0700

    Release 0.3.2

--- src/lib.rs
+++ src/lib.rs
@@ -210,7 +210,7 @@
 //! # fn main() {}
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/gflags/0.3.1")]
+#![doc(html_root_url = "https://docs.rs/gflags/0.3.2")]
 
 macro_rules! eprintln {
     ($($tt:tt)*) => {{
