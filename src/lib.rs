76f53916bd14355a217ef0b910e6f87b875f36ff
David Tolnay <dtolnay@gmail.com>
2021-11-09 21:01:59 -0800

    Release 0.3.9

--- src/lib.rs
+++ src/lib.rs
@@ -218,7 +218,7 @@
 //! # fn main() {}
 //! ```
 
-#![doc(html_root_url = "https://docs.rs/gflags/0.3.8")]
+#![doc(html_root_url = "https://docs.rs/gflags/0.3.9")]
 #![allow(
     clippy::cast_possible_truncation,
     clippy::let_underscore_drop,
